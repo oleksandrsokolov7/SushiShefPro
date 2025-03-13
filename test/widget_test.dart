00564313db5734bc3f6c3fac264ecea9212df168
oleksandrsokolov7 <oleksandrsokolov788@gmail.com>
2025-03-13 07:53:14 -0700

    versoin update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pidkazki2/main.dart';
+import 'package:sushi_shef_asistant/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
